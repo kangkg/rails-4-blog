d654a16bf0d46f3e7fe007136d69bd2bcaca5417
Kenneth G. Kang <kenneth.g.kang@vanderbilt.edu>
2016-12-04 17:21:06 -0600

    user login

---
+++ dbc-blog/app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.find_by(username: params[:username])
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
+			redirect_to '/'
+		else
+			render :new
+		end
+	end
+
+
+end
